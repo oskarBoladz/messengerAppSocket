64b05eccfe42271e7fa5dfe7c9b04fe606aa6b5b
oskarBoladz <103318195+oskarBoladz@users.noreply.github.com>
2022-07-08 17:36:27 +0200

    huj1

---
+++ app/script.js
@@ -0,0 +1,32 @@
+const socket = io('ws://localhost:8080');
+
+nick=document.getElementById('nick').value
+
+socket.on('message',mesg=>{
+    if(mesg[1]!=nick){
+    div = document.createElement('div');
+    div.className="hismessage"
+    div.innerHTML=mesg[0];
+    document.getElementById('allMessagesUwU').appendChild(div)
+}
+})
+
+document.querySelector('button').onclick = ()=>{
+
+    nick=document.getElementById('nick').value
+    text = document.querySelector('input').value;
+    const div = document.createElement('div');
+
+    div.className="mymessage"
+    div.innerHTML="["+nick+"]: "+text;
+    document.getElementById('allMessagesUwU').appendChild(div)
+    
+    socket.emit('message',["["+nick+"]: "+text,nick])
+
+    document.getElementById('mesage').value=''
+
+}
+/*const div = document.createElement('div');
+    div.className="mymessage"
+    div.innerHTML="["+nick+"]: "+text;
+    document.getElementById('allMessagesUwU').appendChild(div)*/
---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http').createServer();
+
+const io = require('socket.io')(http,{
+    cors:{orygin:"*"}
+})
+
+io.on('connection',(socket)=>{
+    console.log('connected')
+    socket.on('message',(message)=>{
+        console.log(message)
+        io.emit('message',message)
+    })
+})
+
+http.listen(8080,()=>console.log("slucham"))
